d9740ece5a67f22faf5d69e0dbd8c5023b629aa8
sleepmx <58004887+sleepmx@users.noreply.github.com>
2019-11-30 23:47:01 +0300

    Update discord.js

--- discord.js
+++ discord.js
@@ -24,6 +24,7 @@ client.on('ready', () => {
     //статус бота
     client.user.setStatus('dnd') //статус не беспокоить. На выбор (Online, dnd, invisible, idle)
     client.user.setGame('bruh moments', 'http://www.twitch.tv/sleep_mo')
+    
 });
     
 client.on('guildMemberAdd', member => {
@@ -42,4 +43,27 @@ client.on('guildMemberRemove', member => {
     memberCountChannel.setName('Участников: ' + memberCount)
     .then(result => console.log(result))
     .catch(error => console.log(error));
+    
+client.on('message', message => {
+    
+    let args = message.content.substring(prefix.length).split(" ");
+    
+    switch(args[0]){
+       case 'hug' :
+            var hug = ["https://media.giphy.com/media/143v0Z4767T15e/giphy.gif","https://media.giphy.com/media/BXrwTdoho6hkQ/giphy.gif","https://media.giphy.com/media/kvKFM3UWg2P04/giphy.gif","https://media.giphy.com/media/od5H3PmEG5EVq/giphy.gif", "https://media.giphy.com/media/lrr9rHuoJOE0w/giphy.gif", "https://media.giphy.com/media/JTjSlqiz63j5m/giphy.gif", "https://media.giphy.com/media/VGACXbkf0AeGs/giphy.gif", "https://media.giphy.com/media/ddGxYkb7Fp2QRuTTGO/giphy.gif", "https://media.giphy.com/media/QpAkCCGu2saR2/giphy.gif", "https://media.giphy.com/media/nUz7d1sUppGta/giphy.gif"]
+            var pic = Math.floor((Math.random() * hug.length))
+            let xuser = message.mentions.users.first()
+            var hcolor = ["0xFF3762","0xFF7A37","37FF9B"] //переменная отвечающая за цвета
+            var col = Math.floor((Math.random() * hcolor.length)) //переменная отвечающая за рандом
+            if (!xuser) return message.channel.send("Укажи, кого хочешь обнять :heart:")
+            const HugEmbed = new Discord.RichEmbed()
+            .setDescription(xuser + ", " + message.author + " обнял тебя :heart_eyes:")
+            .setColor(hcolor[col]) // рандомный цвет
+            .setImage(hug[pic])
+            .setFooter("NOT FROM PARIS MADAME", "https://media.giphy.com/media/zqIW9ievcWOQg/giphy.gif")
+            message.channel.send(HugEmbed)
+        break;
+    }
+})
+    
 });
